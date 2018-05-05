46b6436636ddc628317de16a518ddc3b585e0ece
Renee' <A2icess@gmail.com>
2018-05-04 21:09:22 -0400

    First test file

---
+++ src/javacollectionapplication/JavaCollectionApplication.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javacollectionapplication;
+
+/**
+ *
+ * @author Renee
+ */
+public class JavaCollectionApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int var1; // this declares a variable
+        int var2; // this declare another variable
+    }
+    
+}
